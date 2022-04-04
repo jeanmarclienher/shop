b7eb4c73af37ab55fa3fe2e5dbb1d442eec9eda5
jml <jml@Mac-mini-de-Jean-marc.local>
2022-04-04 12:46:24 +0200

    1

---
+++ index.php
@@ -0,0 +1,130 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Language" content="fr-ch">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>O'ksi'D</title>
+</head>
+
+<body background="images/backgound.jpg" style="background-attachment: fixed">
+
+<table border="0" cellpadding="3" cellspacing="0" width="100%" height="93%">
+	<!-- MSTableType="layout" -->
+	<tr>
+		<td valign="top">
+		<!-- MSCellType="DecArea" -->
+		<a href="http://www.oksid.ch">
+		<img border="0" src="images/oksid.gif" alt="O'ksi'D Home page"></a></td>
+		<td valign="top">
+		<!-- MSCellType="ContentHead" -->
+		<table cellpadding="0" cellspacing="0">
+			<!-- MSTableType="layout" -->
+			<tr>
+				<td align="center" width="100%">
+				<h2 align="center">&nbsp;Simple electronic solutions for a complex 
+				world</h2>
+				</td>
+				<td valign="top" width="45"><a href="index_fr.html">
+				<img border="0" src="images/fr.gif" width="30" height="15" alt="Français"></a>&nbsp;</td>
+				<td height="85" valign="top" width="46"><a href="index_en.html">
+				<img border="0" src="images/en.gif" width="30" height="15" alt="English"></a></td>
+			</tr>
+		</table>
+		</td>
+	</tr>
+	<tr>
+		<td valign="top" width="168">
+		<!-- MSCellType="NavBody" -->
+		&nbsp;<p><u><b><font face="Arial">Computer :</font></b></u></p>
+		<p><font face="Arial">
+		<a title="Computer programming basic tutorial C, C++, Perl..." href="fr/prog_int/">Intro à la prog.</a>
+		<img border="0" src="images/fr.gif" width="30" height="15"><br>
+		<a title="Unix basic tutorial" href="fr/unix/">Intro à Unix</a>
+		<img border="0" src="images/fr.gif" width="30" height="15"><br>
+		<a title="Some tips about Linux configuration" href="fr/config/">Config. de Linux</a>
+		<img border="0" src="images/fr.gif" width="30" height="15"><br>
+		<a title="Graphical LInux environnement" href="xd640/">Xd640</a> <br>
+		<a title="Unicode library" href="xutf8/">Xutf8</a> <br>
+		<a title="Graphical version of Newt" href="gnewt/">Gnewt</a><br>
+		<a title="International FLTK" href="fltk-utf">FLTK-UTF8</a> <br>
+		<a title="SF2 VST plug-in" href="vst/">SoundFont Player </a><br>
+		<a title="Misc softwares" href="misc/">Misc</a> </font></p><p><u><b>
+		<font face="Arial">Electronic :</font></b></u><p>
+		<font face="Arial">
+		<a title="DMX 512 parallel port interface" href="spp2dmx/">OksiD DMX 3/1</a></font></p>
+		<p><u><font face="Arial"><b>Contact:</b></font></u></p>
+		<p><font face="Arial"><a title="French/English forums" href="phpBB2/">Forums</a><br>
+		<a title="How to reach us..." href="contact_en.html">Contact</a></font></p>
+		<p>&nbsp;</td>
+		<td valign="top" width="100%" >
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="image" src="https://www.paypal.com//en_US/i/btn/sc-but-03.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+<input type="hidden" name="add" value="1">
+<input type="hidden" name="cmd" value="_cart">
+<input type="hidden" name="business" value="oksid@bluewin.ch">
+<input type="hidden" name="item_name" value="compact USB to DMX 512 interface">
+<input type="hidden" name="item_number" value="USB2DMX-1">
+<input type="hidden" name="amount" value="80.00">
+<input type="hidden" name="no_shipping" value="2">
+<input type="hidden" name="return" value="http://www.oksid.ch/shop/successful.php">
+<input type="hidden" name="cancel_return" value="http://www.oksid.ch/shop/cancel.php">
+<input type="hidden" name="no_note" value="1">
+<input type="hidden" name="currency_code" value="EUR">
+<input type="hidden" name="handling" value="0" />
+<input type="hidden" name="shipping" value="0" />
+
+</form>
+
+
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="image" src="https://www.paypal.com//en_US/i/btn/sc-but-03.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+<input type="hidden" name="add" value="1">
+<input type="hidden" name="cmd" value="_cart">
+<input type="hidden" name="business" value="oksid@bluewin.ch">
+<input type="hidden" name="item_name" value="USB DMX 512 input">
+<input type="hidden" name="item_number" value="DMXIN2USB-1">
+<input type="hidden" name="amount" value="120.00">
+<input type="hidden" name="page_style" value="Primary">
+<input type="hidden" name="no_shipping" value="2">
+<input type="hidden" name="return" value="http://www.oksid.ch/shop/successful.php">
+<input type="hidden" name="cancel_return" value="http://www.oksid.ch/shop/cancel.php">
+<input type="hidden" name="no_note" value="1">
+<input type="hidden" name="currency_code" value="EUR">
+</form>
+
+
+
+
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_cart">
+<input type="hidden" name="business" value="oksid@bluewin.ch">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/view_cart.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+<input type="hidden" name="display" value="1">
+</form>
+
+
+
+
+
+
+
+		
+		<p align="center">&nbsp;</td>
+	</tr>
+</table>
+<br />
+<hr noshade size="2" width="50%" />
+<center><font size="2">Copyright (c) 2000-2004 Jean-Marc Lienher</font></center>
+</body>
+
+</html>
---
+++ successful.php
@@ -0,0 +1,74 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Language" content="fr-ch">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>O'ksi'D</title>
+</head>
+
+<body background="images/backgound.jpg" style="background-attachment: fixed">
+
+<table border="0" cellpadding="3" cellspacing="0" width="100%" height="93%">
+	<!-- MSTableType="layout" -->
+	<tr>
+		<td valign="top">
+		<!-- MSCellType="DecArea" -->
+		<a href="http://www.oksid.ch">
+		<img border="0" src="images/oksid.gif" alt="O'ksi'D Home page"></a></td>
+		<td valign="top">
+		<!-- MSCellType="ContentHead" -->
+		<table cellpadding="0" cellspacing="0">
+			<!-- MSTableType="layout" -->
+			<tr>
+				<td align="center" width="100%">
+				<h2 align="center">&nbsp;Simple electronic solutions for a complex 
+				world</h2>
+				</td>
+				<td valign="top" width="45"><a href="index_fr.html">
+				<img border="0" src="images/fr.gif" width="30" height="15" alt="Français"></a>&nbsp;</td>
+				<td height="85" valign="top" width="46"><a href="index_en.html">
+				<img border="0" src="images/en.gif" width="30" height="15" alt="English"></a></td>
+			</tr>
+		</table>
+		</td>
+	</tr>
+	<tr>
+		<td valign="top" width="168">
+		<!-- MSCellType="NavBody" -->
+		&nbsp;<p><u><b><font face="Arial">Computer :</font></b></u></p>
+		<p><font face="Arial">
+		<a title="Computer programming basic tutorial C, C++, Perl..." href="fr/prog_int/">Intro à la prog.</a>
+		<img border="0" src="images/fr.gif" width="30" height="15"><br>
+		<a title="Unix basic tutorial" href="fr/unix/">Intro à Unix</a>
+		<img border="0" src="images/fr.gif" width="30" height="15"><br>
+		<a title="Some tips about Linux configuration" href="fr/config/">Config. de Linux</a>
+		<img border="0" src="images/fr.gif" width="30" height="15"><br>
+		<a title="Graphical LInux environnement" href="xd640/">Xd640</a> <br>
+		<a title="Unicode library" href="xutf8/">Xutf8</a> <br>
+		<a title="Graphical version of Newt" href="gnewt/">Gnewt</a><br>
+		<a title="International FLTK" href="fltk-utf">FLTK-UTF8</a> <br>
+		<a title="SF2 VST plug-in" href="vst/">SoundFont Player </a><br>
+		<a title="Misc softwares" href="misc/">Misc</a> </font></p><p><u><b>
+		<font face="Arial">Electronic :</font></b></u><p>
+		<font face="Arial">
+		<a title="DMX 512 parallel port interface" href="spp2dmx/">OksiD DMX 3/1</a></font></p>
+		<p><u><font face="Arial"><b>Contact:</b></font></u></p>
+		<p><font face="Arial"><a title="French/English forums" href="phpBB2/">Forums</a><br>
+		<a title="How to reach us..." href="contact_en.html">Contact</a></font></p>
+		<p>&nbsp;</td>
+		<td valign="top" width="100%" >
+Per the user agreement, you must provide verbiage on the page displayed by the Return URL that will help the buyer understand that the payment has been made and that the transaction has been completed. 
+<br>
+You must provide verbiage on the page displayed by the Return URL that explains that payment transaction details will be emailed to the buyer. 
+<br>
+Example: Thank you for your payment. Your transaction has been completed, and a receipt for your purchase has been emailed to you. You may log into your account at www.paypal.com/ch to view details of this transaction. 
+
+		<p align="center">&nbsp;</td>
+	</tr>
+</table>
+<br />
+<hr noshade size="2" width="50%" />
+<center><font size="2">Copyright (c) 2000-2004 Jean-Marc Lienher</font></center>
+</body>
+
+</html>
